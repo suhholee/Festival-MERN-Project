0c97ea0275163b2da7ecc080f825f5cb2b89478f
Thom Clegg <tomclegg25@gmail.com>
2023-03-23 15:50:09 +0000

    tom artists complete

--- db/data/artists.js
+++ db/data/artists.js
@@ -1,62 +1,75 @@
+
 [
   {
     name: 'Big Youth',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=g6opnZQCR3E',
+    image: '../../assets/big-youth.jpg',
   },
   {
     name: 'Sister Nancy',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=qXnT3LFTc-s',
+    image: '../../assets/sister-nancy.png',
   },
   {
     name: 'Dawn Penn',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=j4vjVetZPpM',
+    image: '../../assets/dawn-penn.jpeg',
   },
   {
     name: 'Eek-A-Mouse',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=VWxwbXLCPnc',
+    image: '../../assets/Eek-a-Mouse.jpeg',
   },
   {
     name: 'Max Romeo',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=H9oLSMSW4SI',
+    image: '../../assets/max-romeo.jpeg',
   },
   {
     name: 'Channel One Soundsystem',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=0mC8CFwG57w',
+    image: '../../assets/channel-one.jpeg',
   },
   {
     name: 'Iration Steppas',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=bRBjWPnRr4o',
+    image: '../../assets/Iration_Steppas-250.jpg',
   },
   {
     name: 'Mungos Hifi Soundsystem',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=zr5-50ZEQs4',
+    image: '../../assets/mungos hifi.jpeg',
   },
   {
     name: 'General Levy',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=mL2Bgj-za5k',
+    image: '../../assets/general-levy.jpeg',
   },
   {
     name: 'Shy FX',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=es3RMEBx0pg',
+    image: '../../assets/shy-fx.jpg',
   },
   {
     name: 'Dj Marky',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=5xVjcDTRYG8',
+    image: '../../assets/Dj Marky.jpeg',
   },
   {
     name: 'Break',
     stage: 'jungle constellation',
     url: 'https://www.youtube.com/watch?v=btocybienAY',
+    image: '../../assets/Break.png',
   }
 ]
