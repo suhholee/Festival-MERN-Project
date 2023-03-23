fbd8927354f595b2f5a61797fcd36fead2727768
Thom Clegg <tomclegg25@gmail.com>
2023-03-23 14:43:45 +0000

    artist data populated

---
+++ db/data/artists.js
@@ -0,0 +1,62 @@
+[
+  {
+    name: 'Big Youth',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=g6opnZQCR3E',
+  },
+  {
+    name: 'Sister Nancy',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=qXnT3LFTc-s',
+  },
+  {
+    name: 'Dawn Penn',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=j4vjVetZPpM',
+  },
+  {
+    name: 'Eek-A-Mouse',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=VWxwbXLCPnc',
+  },
+  {
+    name: 'Max Romeo',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=H9oLSMSW4SI',
+  },
+  {
+    name: 'Channel One Soundsystem',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=0mC8CFwG57w',
+  },
+  {
+    name: 'Iration Steppas',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=bRBjWPnRr4o',
+  },
+  {
+    name: 'Mungos Hifi Soundsystem',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=zr5-50ZEQs4',
+  },
+  {
+    name: 'General Levy',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=mL2Bgj-za5k',
+  },
+  {
+    name: 'Shy FX',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=es3RMEBx0pg',
+  },
+  {
+    name: 'Dj Marky',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=5xVjcDTRYG8',
+  },
+  {
+    name: 'Break',
+    stage: 'jungle constellation',
+    url: 'https://www.youtube.com/watch?v=btocybienAY',
+  }
+]
