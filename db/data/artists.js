bf7053ccbff019e28926747113e8f381e6385f59
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-03-23 15:56:28 +0000

    Added artist database

---
+++ db/data/artists.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    name: 'Marie Malarie',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=QteCoRkPmg8',
+    image: '../assets/AvalonEmerson.jpeg',
+  },
+  {
+    name: 'JeonQ',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=5C0BBBeNVhA',
+    image: '../assets/JeonQ.png',
+  },
+  {
+    name: 'KI/KI',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=fR_5ivov33g',
+    image: '../assets/KI:KI.png',
+  },
+  {
+    name: 'Avalon Emerson',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=Sj_X1l9mcdE',
+    image: '../assets/AvalonEmerson.jpeg',
+  },
+  {
+    name: 'Cosmic Caz',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=3-hhO4p06ro',
+    image: '../assets/CosmicCaz.jpeg',
+  },
+  {
+    name: 'dj fart in the club',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=KlBYTu0v1B8',
+    image: '../assets/djfartintheclub.jpeg',
+  },
+  {
+    name: 'R.I.M',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=nOVe85_RKVU',
+    image: '../assets/R.I.M.png',
+  },
+  {
+    name: 'Hagop Tchaparian',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=pi6GRzhZ8cs',
+    image: '../assets/HagopTchaparian.png',
+  },
+  {
+    name: 'Daniel Avery',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=Kz2mJ6BBVLU',
+    image: '../assets/DanielAvery.png',
+  },
+  {
+    name: 'Marcus L',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=cXzrE4csAAc',
+    image: '../assets/MarcusL.png',
+  },
+  {
+    name: 'MCMLXXXV',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=iStj1bR1NDs',
+    image: '../assets/MCMLXXXV.png',
+  },
+  {
+    name: 'Call Super',
+    stage: 'Techno',
+    url: 'https://www.youtube.com/watch?v=erAR5S0mBsU',
+    image: '../assets/CallSuper.png',
+  }
+]
