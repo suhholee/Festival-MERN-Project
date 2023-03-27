086834b43208dc3f311854322a9a6236ff508536
Thom Clegg <tomclegg25@gmail.com>
2023-03-27 10:34:43 +0100

    tom merged with dev monday morning

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -34,6 +34,7 @@ const Register = () => {
       navigate('/login')
     } catch (error) {
       console.log('console', error)
+      // if (error)
       setError(error.response.data.message)
     }
   }
