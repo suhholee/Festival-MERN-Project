f0491c22c061013aa762fdf88326c99910ac2c9b
Thom Clegg <tomclegg25@gmail.com>
2023-03-30 10:28:50 +0100

    stage names attached

--- client/src/components/main/Profile.js
+++ client/src/components/main/Profile.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect,Fragment } from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 // Custom imports
 import { authenticated,isAuthenticated } from '../helpers/auth'
+import { Error } from '../common/Error'
 
 const Profile = () => {
   const { userId } = useParams()
   const navigate = useNavigate()
 
-  const [user, setUser] = useState({})
-  const [userComments, setUserComments] = useState([])
+  const [ user, setUser ] = useState({})
+  const [ userComments, setUserComments ] = useState([])
+  const [ stages,setStages ] = useState([])
+  const [ error,setError ] = useState(null)
 
   // ! On Mount
   useEffect(() => {
@@ -21,25 +24,24 @@ const Profile = () => {
         setUser({ ...data })
       } catch (error) {
         console.log(error)
+        setError(error.response)
       }
     }
     const getComments = async () => {
       try {
         const { data } = await axios.get('/api/stages')
-        console.log('stages->', data)
+        setStages(data)
         const comments = data.map(stage => stage.comments)
-        console.log(comments)
         const userComments = comments.map(comments => comments.filter(comment => comment.owner._id === userId))
-        console.log('user comments', userComments)
         setUserComments(userComments)
       } catch (error) {
         console.log(error)
+        setError(error.response)
       }
     }
     getComments()
     getUser()
   }, [])
-
   return (
     <>
       <h1> Profile </h1>
@@ -50,14 +52,22 @@ const Profile = () => {
       <div>
         {userComments.map((stage, i) => {
           if (stage.length > 0) {
-            return stage.map((comment,i) => {
-              console.log(comment.text)
-              return <p key={i}>{comment.text}</p>
-            } )
-            // console.log(stage)
+            return (
+              <div key={i}>
+                <h2> {stages[i].name}</h2>
+                <div>
+                  {stage.map((comment,i) => {
+                    return <p key={i}>{comment.text}</p>
+                  } )}
+                </div>
+              </div>
+            )
           } else {
             return (
-              <p key={i}> No Comment</p>
+              <div key={i}>
+                <h2>{stages[i].name}</h2>
+                <p key={i}> No Comment</p>
+              </div>  
             )
           }
         })}
